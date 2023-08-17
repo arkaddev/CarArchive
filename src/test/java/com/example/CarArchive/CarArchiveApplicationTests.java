157314365683a938f46453688f2b4173f6183efd
arkaddev <arek1994mr@gmail.com>
2023-08-17 20:49:28 +0200

    init commit

---
+++ src/test/java/com/example/CarArchive/CarArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarArchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
