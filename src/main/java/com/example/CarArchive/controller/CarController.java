0a5fa17500c768e7b9994ca08f128c0863881a10
arkaddev <arek1994mr@gmail.com>
2023-08-29 22:47:26 +0200

    added @Operation to CarController.java

--- src/main/java/com/example/CarArchive/controller/CarController.java
+++ src/main/java/com/example/CarArchive/controller/CarController.java
@@ -4,6 +4,7 @@ import com.example.CarArchive.model.Car;
 import com.example.CarArchive.model.Part;
 import com.example.CarArchive.repository.CarRepository;
 import com.example.CarArchive.service.CarService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,22 +21,25 @@ public class CarController {
         this.carService = carService;
     }
 
-
+    @Operation(summary = "get all cars", description = "")
     @GetMapping("/cars")
     public List<Car> getAllCars() {
         return carService.getAllCars();
     }
 
+    @Operation(summary = "get car by id", description = "")
     @GetMapping("/car/{id}")
     public Car getCarById(@PathVariable int id) {
         return carService.getCarById(id);
     }
 
+    @Operation(summary = "add new car", description = "")
     @PostMapping("/cars")
     public Car addNewCar(@RequestBody Car car) {
         return carService.addNewCar(car);
     }
 
+    @Operation(summary = "delete car by id", description = "")
     @DeleteMapping("/car/{id}")
     public void deleteCar(@PathVariable int id){
         carService.deleteCar(id);
