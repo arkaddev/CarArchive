7e70e55b90af165e935b2f882e01bc277462e78c
arkaddev <arek1994mr@gmail.com>
2023-09-13 22:29:58 +0200

    addNewUser refactor

---
+++ src/main/java/com/example/CarArchive/dto/UserRequest.java
@@ -0,0 +1,33 @@
+package com.example.CarArchive.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
