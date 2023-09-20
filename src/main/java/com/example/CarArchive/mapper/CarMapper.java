abca58d3682718ef85dd8f739e5b5bf2189977ce
arkaddev <arek1994mr@gmail.com>
2023-09-20 08:40:09 +0200

    CarMapperImpl.java was created

--- src/main/java/com/example/CarArchive/mapper/CarMapper.java
+++ src/main/java/com/example/CarArchive/mapper/CarMapper.java
@@ -3,14 +3,13 @@ package com.example.CarArchive.mapper;
 import com.example.CarArchive.dto.CarRequest;
 import com.example.CarArchive.dto.CarResponse;
 import com.example.CarArchive.model.Car;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 
-@Mapper(componentModel = "spring")
+
+
 public interface CarMapper {
-    @Mapping(source = "ownerId", target = "user.id")
+    //@Mapping(source = "ownerId", target = "user.id")
     Car carRequestToCar(CarRequest carRequest);
-    @Mapping(source = "user.id", target = "ownerId")
+    //@Mapping(source = "user.id", target = "ownerId")
     CarResponse carToCarResponse(Car car);
 }
