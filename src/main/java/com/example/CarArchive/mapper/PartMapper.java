21809da5708cb0436514557eef3d19257c2f7fd2
arkaddev <arek1994mr@gmail.com>
2023-09-20 08:33:45 +0200

    PartMapperImpl.java was created

--- src/main/java/com/example/CarArchive/mapper/PartMapper.java
+++ src/main/java/com/example/CarArchive/mapper/PartMapper.java
@@ -2,12 +2,11 @@ package com.example.CarArchive.mapper;
 
 import com.example.CarArchive.dto.PartResponse;
 import com.example.CarArchive.model.Part;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+
+//@Mapper(componentModel = "spring")
 public interface PartMapper {
-    @Mapping(source = "car.id", target = "carId")
+    //@Mapping(source = "car.id", target = "carId")
     PartResponse partToPartResponse(Part part);
 
 }
